48f444144ea3a3a5bc353a95f64d3576bad365c7
Waller <awaller2@ead.ubc.ca>
2019-03-08 15:59:28 -0800

    Finished

--- Assets/Scripts/Printer.cs
+++ Assets/Scripts/Printer.cs
@@ -1,16 +1,136 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+using Image = UnityEngine.UI.Image;
 
-public class Printer : MonoBehaviour {
+public class Printer : MonoBehaviour, ITrackableEventHandler
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private GameObject imageTarget;
+    [SerializeField]
+    private GameObject canvas;
+
+    [SerializeField]
+    private Image lSensor;
+    [SerializeField]
+    private Image fSensor;
+    [SerializeField]
+    private Image direction;
+
+    [SerializeField]
+    private Text lText;
+    [SerializeField]
+    private Text fText;
+    [SerializeField]
+    private Text dText;
+
+    public int front;
+    public int left;
+    public string dir;
+
+    [SerializeField]
+    private int threshold;
+
+    private Color initColor;
+
+    private TrackableBehaviour mTrackableBehaviour;
+    private bool targetFound;
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            targetFound = true; //when target is found
+        }
+        else
+        {
+            targetFound = false; //when target is lost
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        initColor = lSensor.color;
+        mTrackableBehaviour = imageTarget.GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    void Update()
+    {
+        if (targetFound)
+        {
+            canvas.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit(); // exit app if back/esc button is pressed
+            }
+
+
+            SetLeft();
+            SetFront();
+            SetDirection();
+
+
+        }
+
+        if (!targetFound)
+        {
+            canvas.SetActive(false);
+        }
+    }
+
+
+    private void SetLeft()
+    {
+        if(left > 0)
+        {
+            if (left < threshold)
+            {
+                lSensor.color = Color.red;
+
+            }
+            else
+            {
+                lSensor.color = initColor;
+            }
+            lText.text = left.ToString();
+        }
+        
+    }
+    private void SetFront()
+    {
+        if(front > 0)
+        {
+            if (front < threshold)
+            {
+                fSensor.color = Color.red;
+
+            }
+            else
+            {
+                fSensor.color = initColor;
+            }
+            fText.text = front.ToString();
+        }
+        
+    }
+
+    private void SetDirection()
+    {
+        if (!string.IsNullOrEmpty(dir))
+        {
+            dText.text = dir;
+        }
+    }
 }
--- Assets/Scripts/ReadStream.cs
+++ Assets/Scripts/ReadStream.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 
 public class ReadStream : MonoBehaviour
 {
+    private Printer printer;
     [SerializeField]
     private string photonUrl;
     WebStreamReader request = null;
 
+    FrontSensorData parseFront = new FrontSensorData();
+    LeftSensorData parseLeft = new LeftSensorData();
+    DirectionData parseDirection = new DirectionData();
+
     void Start()
     {
+        printer = this.GetComponent<Printer>();
         StartCoroutine(WRequest());
     }
 
@@ -29,7 +35,28 @@ public class ReadStream : MonoBehaviour
                 {
                     if (!string.IsNullOrEmpty(data[i]))
                     {
-                        //Parse text here
+                        if (data[i].Contains("Direction"))
+                        {
+                            Debug.Log(data[i]);
+                            string output = data[i].Substring(data[i].IndexOf("{"));
+                            Debug.Log(output);
+                            parseDirection = JsonUtility.FromJson<DirectionData>(output);
+                            Debug.Log(parseDirection.data);
+                            printer.dir = parseDirection.data;
+                        }
+                        if (data[i].Contains("leftDistance"))
+                        {
+                            string output = data[i].Substring(data[i].IndexOf("{"));
+                            parseLeft = JsonUtility.FromJson<LeftSensorData>(output);
+                            printer.left = parseLeft.data;
+                        }
+                        if (data[i].Contains("frontDistance"))
+                        {
+                            string output = data[i].Substring(data[i].IndexOf("{"));
+                            parseFront = JsonUtility.FromJson<FrontSensorData>(output);
+                            printer.front = parseFront.data;
+                        }
+
                     }
                 }
             }
@@ -47,5 +74,18 @@ public class ReadStream : MonoBehaviour
     {
         Debug.Log("Received new amount: " + aAmount);
         // Do whatever you want with the value
+    }
+
+    public class FrontSensorData
+    {
+        public int data;
+    }
+    public class LeftSensorData
+    {
+        public int data;
+    }
+    public class DirectionData
+    {
+        public string data;
     }
 }
